f2e3dd07693150d8c7e78a13d5a144d30b05d643
Luis Valencia <luvle@hey.com>
2022-12-22 15:45:06 -0800

    Added Fonts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -17,17 +17,26 @@ export default {
 </script>
 
 <template>
-  <main class="grid-item"
-  :class="classObject">
+  <main class="grid-item" :class="classObject">
     <h1>Luis Valencia</h1>
-    <p>Software Engineer in Sunny SoCal</p>
-    <font-awesome-icon class="construction-icon" icon="fa-solid fa-person-digging" /><span>Site Under Construction</span>
+    <h2>Southern California <span class="italicized regular">Software Engineer</span></h2>
+    <p>
+      <font-awesome-icon class="construction-icon" icon="fa-solid fa-person-digging" /><span>Site Under
+        Construction</span>
+    </p>
     <p><strong>State:</strong> Broke Ground</p>
   </main>
 </template>
 
 <style scoped lang="scss">
-.construction-icon + span {
+.construction-icon+span {
   margin-left: 8px;
+}
+
+h1 {
+  margin-bottom: 0em;
+  &+h2 {
+    margin-top: 0em;
+  }
 }
 </style>
